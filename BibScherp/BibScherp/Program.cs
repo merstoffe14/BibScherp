c224cd8d89ad723004924caa9831ace72197ff60
merstoffe14 <merlijn.qm@hotmail.com>
2017-11-16 18:17:41 +0100

    Hello world

---
+++ BibScherp/BibScherp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibScherp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello maan");
+        }
+    }
+}
